49eb21e1de69460f797851519122763730c080ce
sauravkaushik8 <sauravkaushik8@gmail.com>
2017-12-31 17:54:55 +0530

    Package Init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyensembler',
+  packages = ['pyensembler'],
+  version = '0.1',
+  description = 'Take any number of Machine Learning Models as Base Models and one Machine Learning Model as Top model to produce an ensemble of models in Python.',
+  author = 'Saurav Kaushik',
+  author_email = 'sauravkaushik8@gmail.com',
+  url = 'https://github.com/sauravkaushik8/pyensembler',
+  download_url = 'https://github.com/sauravkaushik8/pyensembler/archive/0.1.tar.gz',
+  keywords = ['ensembling', 'stacking'],
+  classifiers = [],
+)
